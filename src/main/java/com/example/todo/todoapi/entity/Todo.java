ecf163bb8990d1d2f7890d88854cb6c2818bfd15
CYJ <jun940317@gmail.com>
2023-06-20 17:30:41 +0900

    Initial commit

---
+++ src/main/java/com/example/todo/todoapi/entity/Todo.java
@@ -0,0 +1,28 @@
+package com.example.todo.todoapi.entity;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter @ToString
+@EqualsAndHashCode(of = "todoId")
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Entity @Table(name = "tbl_todo")
+public class Todo {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String todoId;
+
+    @Column(nullable = false, length = 30)
+    private String title; //제목
+
+    private  boolean done; //일정 완료 여부
+    private LocalDateTime createDate; //등록시간
+
+}
