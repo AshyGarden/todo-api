9146b9d6096197a37e2950a98ea23ea97ebd81aa
CYJ <jun940317@gmail.com>
2023-06-23 10:53:40 +0900

    20230623 - Todo API todos + user join controller

--- src/main/java/com/example/todo/userapi/service/UserService.java
+++ src/main/java/com/example/todo/userapi/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.todo.userapi.service;
 
+import com.example.todo.exception.DuplicatedEmailException;
+import com.example.todo.exception.NoRegisteredArgumentsException;
 import com.example.todo.userapi.dto.UserSignUpResponseDTO;
 import com.example.todo.userapi.dto.request.UserRequestSignUpDTO;
 import com.example.todo.userapi.entity.User;
@@ -17,16 +19,16 @@ public class UserService {
     private  final UserRepository userRepository;
     private  final PasswordEncoder encoder;
 
-    public UserSignUpResponseDTO create(final UserRequestSignUpDTO dto){
+    public UserSignUpResponseDTO create(final UserRequestSignUpDTO dto) throws RuntimeException {
 
         String email = dto.getEmail();
         if(dto == null){
-            throw new RuntimeException("가입 정보가 없습니다.");
+            throw new NoRegisteredArgumentsException("가입 정보가 없습니다.");
         }
 
         if(userRepository.existsByEmail(email)){ //이메일이 존재하는 경우
             log.warn("이메일이 중복되었습니다. - {}",email);
-            throw new RuntimeException("중복된 이메일 입니다.");
+            throw new DuplicatedEmailException("중복된 이메일 입니다.");
         }
 
         //패스워드 인코딩
@@ -42,7 +44,7 @@ public class UserService {
         return new UserSignUpResponseDTO(saved);
     }
 
-    public boolean isDuplicate(String email) {
+    public boolean isDuplicate(String email) { //중복확인을 위한 함수
         return userRepository.existsByEmail(email);
     }
 }
